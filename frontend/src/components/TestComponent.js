69aa9231619e94937d8677a9a0508f715773e738
BazaUA <vbazalickyi@gmail.com>
2018-08-12 18:39:14 +0300

    add actions fro standart operations

--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
+import { itemsFetch, createItem, deleteItem } from '../actions';
 
 class TestComponent extends Component {
   componentWillMount() {
     this.props.itemsFetch();
+    this.props.deleteItem(3);
   }
 
   // renderRow(item) {
@@ -27,3 +29,4 @@ const mapStateToProps = (state) => {
   return { items: state.items };
 };
 
+export default connect(mapStateToProps, { itemsFetch, createItem, deleteItem })(TestComponent);
